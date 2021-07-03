5e788adc747bcddc8fa37a9032d0277d726d412d
BillXSY <714017703@qq.com>
2021-07-03 14:06:58 +0800

    0703

--- project3/card.h
+++ project3/card.h
@@ -1,7 +1,10 @@
 #ifndef __CARD_H__
 #define __CARD_H__
+
 #include <string>
+
 using namespace std;
+
 enum Suit {
     SPADES, HEARTS, CLUBS, DIAMONDS
 };
@@ -15,13 +18,14 @@ enum Team {
     SOSBrigade, StardustCrusaders
 };
 
-extern const string SuitNames[DIAMONDS + 1];
-extern const string SpotNames[ACE+1];
+extern const string SuitNames[DIAMONDS + 1]; //size = 4
+extern const string SpotNames[ACE + 1]; //size = 13
 extern const string SOS_Name[5];
 extern const string SC_Name[5];
+
 struct Card {
-    Spot  spot;
-    Suit  suit;
+    Spot spot;//点数
+    Suit suit;//花色
 };
 
 // void displayCard(Card card);
