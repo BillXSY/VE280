675d922ee1bfaa3d09333928bc6e4e2ca0d30d41
BillXSY <714017703@qq.com>
2021-07-04 13:58:39 +0800

    Hand Interface

---
+++ project3/hand.cpp
@@ -0,0 +1,42 @@
+//
+// Created by 71401 on 2021/7/3.
+//
+
+#include "hand.h"
+
+Hand::Hand() {
+    this->curValue.count = 0;
+    this->curValue.soft = false;
+}
+
+void Hand::discardAll() {
+    this->curValue.count = 0;
+    this->curValue.soft = false;
+}
+
+void Hand::addCard(Card c) {
+    if (c.spot < 9) {
+        this->curValue.count += (c.spot + 2);
+    } else if (c.spot >= 9 && c.spot <= 11) {
+        this->curValue.count += 10;
+    } else if (c.spot == 12) {
+        this->curValue.count += 1;
+        this->curValue.soft = true;
+    }
+}
+
+HandValue Hand::handValue() const {
+    if (!curValue.soft) {
+        return curValue;
+    } else {
+        HandValue AceValue;
+        if (this->curValue.count < 12) {
+            AceValue.count = curValue.count + 10;
+            AceValue.soft = true;
+        } else if (this->curValue.count > 12) {
+            AceValue.count = curValue.count;
+            AceValue.soft = false;
+        }
+        return AceValue;
+    }
+}
