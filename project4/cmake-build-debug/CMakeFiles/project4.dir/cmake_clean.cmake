eb53d624857b4266dbeb0cad897efa16e55c7834
BillXSY <714017703@qq.com>
2021-07-12 19:27:13 +0800

    0712

---
+++ project4/cmake-build-debug/CMakeFiles/project4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project4.dir/BinaryTree.cpp.o"
+  "CMakeFiles/project4.dir/dbc.cpp.o"
+  "CMakeFiles/project4.dir/test.cpp.o"
+  "project4"
+  "project4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
