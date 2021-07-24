1c51918db329de3963ca121a21cd6f856dfe13db
BillXSY <714017703@qq.com>
2021-07-24 15:39:07 +0800

    test for Dlist Implement

--- project5/test.cpp
+++ project5/test.cpp
@@ -2,21 +2,40 @@
 #include "Dlist.h"
 using namespace std;
 
+void DlistPrinter(Dlist<int> &l) {
+    while (!l.isEmpty()) {
+        cout << *l.removeFront() << ' ';
+    }
+    cout << endl;
+}
+
+
+
 int main(int argc, char *argv[])
 {
     int result = 0;
 
     Dlist<int> ilist;
-    int *ip = new int(1);
-    ilist.insertFront(ip);
+    ilist.removeAll();
 
-    ip = ilist.removeFront();
-    if(*ip != 1)
-        result = -1;
-    delete ip;
+    int *ip1 = new int(1);
+    ilist.insertBack(ip1);
+    int *ip2 = new int(2);
+    ilist.insertBack(ip2);
+    int *ip3 = new int(3);
+    ilist.insertBack(ip3);
 
-    if(!ilist.isEmpty())
-        result = -1;
+    int *ip4 = new int(4);
+    ilist.insertBack(ip4);
+
+//    ilist.removeAll();
+    Dlist<int> ilist2(ilist);
+//    int *ip5 = new int(5);
+//    ilist2.insertBack(ip5);
+
+    DlistPrinter(ilist);
+    DlistPrinter(ilist2);
+
 
     return result;
 }
